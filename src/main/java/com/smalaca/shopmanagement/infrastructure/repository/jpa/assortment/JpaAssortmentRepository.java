f835134af94313324d9493574983e3eaf50d400c
smalaca <ab13.krakow@gmail.com>
2024-03-19 16:33:52 +0100

    change SecondaryPort annotation into SecondaryAdapter

--- src/main/java/com/smalaca/shopmanagement/infrastructure/repository/jpa/assortment/JpaAssortmentRepository.java
+++ src/main/java/com/smalaca/shopmanagement/infrastructure/repository/jpa/assortment/JpaAssortmentRepository.java
@@ -1,12 +1,13 @@
 package com.smalaca.shopmanagement.infrastructure.repository.jpa.assortment;
 
+import com.smalaca.annotation.architecture.SecondaryAdapter;
 import com.smalaca.annotation.architecture.SecondaryPort;
 import com.smalaca.shopmanagement.domain.assortment.Assortment;
 import com.smalaca.shopmanagement.domain.assortment.AssortmentRepository;
 
 import java.util.UUID;
 
-@SecondaryPort
+@SecondaryAdapter
 public class JpaAssortmentRepository implements AssortmentRepository {
     @Override
     public Assortment findById(UUID assortmentId) {
